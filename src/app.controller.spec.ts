b907deb1a2de5c08702d5a84f5ac14dfc17115ea
YusraSenimAydin <yusrasenimdede@gmail.com>
2024-02-29 22:20:40 +0700

    first commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
+function beforeEach(_arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
+function expect(arg0: string) {
+  throw new Error('Function not implemented.');
+}
