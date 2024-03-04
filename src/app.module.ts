731014b4a470f0176282ef7b6722f36f47599e52
YusraSenimAydin <yusrasenimdede@gmail.com>
2024-03-04 17:09:56 +0700

    user mongodb signup added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NinjasModule } from './ninjas/ninjas.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [NinjasModule, UserModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserSchema } from './users.model';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UserModule {}
