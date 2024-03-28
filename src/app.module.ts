897a5f881e98e3789bebdc8c123027a687860319
YusraSenimAydin <yusrasenimdede@gmail.com>
2024-03-28 22:38:28 +0700

    bug fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,20 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryModule } from './category/category.module';
-import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 import { BillModule } from './bill/bill.module';
 
 @Module({
   imports: [
-    UserModule,
     MongooseModule.forRoot(
       'mongodb+srv://yusrasenimdede:senim8695.@cluster0.3xgnzsa.mongodb.net/pos-database?retryWrites=true&w=majority&appName=Cluster0',
     ),
+    UserModule,
     CategoryModule,
     ProductModule,
     BillModule,
   ],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
