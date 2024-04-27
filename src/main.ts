572d4d02c5c7eeb82ad0a6f14c1f3392e315e6bc
YusraSenimAydin <yusrasenimdede@gmail.com>
2024-04-28 01:15:48 +0700

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './users/users.module';
+import { BillModule } from './bill/bill.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('FIRST NESTJS API')
+    .setDescription('POS API')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Your API Tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [ProductModule, CategoryModule, UserModule, BillModule], //the modules that you want to include in your swagger docs
+  });
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
